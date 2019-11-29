559bbfd66d419e9aa0b26586ade2d71c37b0de61
Berktunc08 <57864551+Berktunc08@users.noreply.github.com>
2019-11-29 22:58:12 +0200

    first commit

---
+++ js/myscript.js
@@ -0,0 +1,23 @@
+
+  function openNav() {
+    document.getElementById("myNav").style.width = "100%"
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%"
+  }
+  
+
+
+
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block"
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none"
+}
+
+
+
